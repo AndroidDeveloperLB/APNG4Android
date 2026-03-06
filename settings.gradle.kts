d0cf9755eeeadd2e58ec1811c4e6ce47fb20ff36
AndroidDeveloperLB <lblb636@gmail.com>
2026-03-06 02:23:44 +0200

    trying to fix issues related to using this library on Jitpack

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,10 +10,12 @@ pluginManagement {
 
 @Suppress("UnstableApiUsage")
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
     repositories {
         google()
         mavenCentral()
+        mavenLocal()
+        maven { url = uri("https://jitpack.io") }
     }
 }
 
